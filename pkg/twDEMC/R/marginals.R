aba4f3a2e4edcb71f8d2327873565122a55033c2
tomaschwutz <tomaschwutz@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-11-23 16:05:35 +0000

    added plot2DKDMarginals
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/twdemc@32 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/twDEMC/R/marginals.R
+++ pkg/twDEMC/R/marginals.R
@@ -49,6 +49,7 @@ marginals1d <- function(
 	## \item{ integrate over all others but 2 column: \code{\link{marginals2d}}  } 
 	## \item{ integrate over all others but 3 column: \code{\link{marginals3d}}  } 
 	## \item{ plotting a 2D marginal distribution: \code{\link{plotMarginal2D}}  } 
+	## \item{ plotting a 2D kernel density estimate: \code{\link{plot2DKDMarginals}}  } 
 	##}
 	
 	xcuts <- cut2(x[,vars],g=n, levels.mean=TRUE)
@@ -133,5 +134,88 @@ attr(marginals3d,"ex") <- function(){
 	}
 	
 }
+
+plot2DKDMarginals <- function(
+	### Evaluates the Kernel-density regression on a grid and displays a figure.
+	form	##<< formula of regression e.g. rLogLik~dO+tvrO
+	,ds		##<< dataframe or matrix with column names corresponding to variables in the formula
+	,dim=40	##<< vector of spaces across predictor space where 
+	,bandwidth = NULL	 ##<< integer vector: bandwidth for each dimension
+		### see adaptive bandwidth of npreg.
+		### about Records to include for estimating bandwidth.
+		### If only one value is supplied, then it is repeated for each predictor
+	,nSampleBandwidth=200 ##<< the sample size to estimate the bandwidth
+		### The larger the sample, the longer it takes
+	,bwtype="adaptive_nn" ,tol=.1, ftol=.1	##<< parameters for \code{\link{npregbw}}
+){
+	##details<<
+	## Uses package snowfall to split calculation to subprocesses.
+	## Requires package np loaded in cluster (sfLibrary(np))
+	
+	if( !is.data.frame(ds) ) ds <- as.data.frame(ds)
+	dsPred <- model.frame(form,ds)
+	nVars <- ncol(dsPred)-1
+	if( nVars != 2) error("plot2DKDMarginals: number of predictor variables must be 2.")
+	if( length(bandwidth) == 1) bandwidth <- rep(bandwidth, nVars)
+	if( length(dim) == 1) dim <- rep(dim, nVars)
+	
+	if( nSampleBandwidth < nrow(ds))
+		ds100 <- ds[sample.int(nrow(ds),nSampleBandwidth),]	# tradeoff 200: larger sample sizes become really slow
+	else
+		ds100 <- ds
+	#bw0 <- npregbw(formula=form, ds100, bwmethod="normal-reference" )
+	#bw3 <- bw30 <- npregbw(formula=form, ds100, bwtype="generalized_nn" ,tol=.1, ftol=.1 )
+	bw3 <- bw30 <- npregbw(formula=form, ds100, bwtype=bwtype ,tol=tol, ftol=ftol )
+	# refine (but does not change)
+	#bw3 <- npregbw(bws=bw30, formula=form, ds100, bwtype="adaptive_nn", tol=0.01, ftol=0.01 )
+	#bw3$bw <- bw30$bw*2
+	#plot(bw32)
+	#est <- npreg(bws=bw3, tdat=dsPred, edat=dsPred)		
+	lMin <- min(dsPred[,1])
+	#sfLibrary(np)
+	tmpf <- function(x,y, bws, minLogLik=lMin){
+		ind <- splitIndices(length(x),4)
+		exdat <- data.frame(x=x,y=y); names(exdat) <- bws$xnames
+		fList <- list(
+			function(bws,dsPred,exdat,ind){ npreg(bws=bws,txdat=dsPred[,-1], tydat=dsPred[,1], exdat=exdat[ind[[1]],]) }
+			,function(bws,dsPred,exdat,ind){ npreg(bws=bws,txdat=dsPred[,-1], tydat=dsPred[,1], exdat=exdat[ind[[2]],]) }
+			,function(bws,dsPred,exdat,ind){ npreg(bws=bws,txdat=dsPred[,-1], tydat=dsPred[,1],exdat=exdat[ind[[3]],]) }
+			,function(bws,dsPred,exdat,ind){ npreg(bws=bws,txdat=dsPred[,-1], tydat=dsPred[,1], exdat=exdat[ind[[4]],]) }
+		)
+		resL <- sfPar(fList,sfParArgsList=list( bws=bws,dsPred=dsPred,exdat=exdat,ind=ind))
+		tmpMean <- do.call(c, lapply( resL, "[[", "mean" ))
+		ifelse( tmpMean>minLogLik, tmpMean, as.numeric(NA))
+	}
+	#tmp2 <- twPlot2DFun( ds$tvrY, ds$tvrO, tmpf, argsFUN=list(bws={tmp<-bw3;tmp$bw=bw3$bw*8;tmp}), xdiv=60, col=rev(heat.colors(20)) )
+	#tmp1 <- twPlot2DFun( ds$tvrY, ds$tvrO, tmpf, argsFUN=list(bws={tmp<-bw3;tmp$bw=c(200,200);tmp}), xdiv=60, col=rev(heat.colors(20)) )
+	#mtrace(tmpf)
+	if( 0==length(bandwidth) ) bandwidth<-bw3$bw
+	tmp2 <- twPlot2DFun( dsPred[,2], dsPred[,3], tmpf, argsFUN=list(bws={tmp<-bw3;tmp$bw=bandwidth;tmp}), xdiv=dim[1], ydiv=dim[2], col=rev(heat.colors(20)), xlab=bw3$xnames[1], ylab=bw3$xnames[2],key.title=title(sub=paste(colnames(dsPred)[1],"\n",sep="")) )
+	names(dimnames(tmp2)) <- bw3$xnames
+	tmp2
+	### Matrix of predictions for the grid.
+}
+attr(plot2DKDMarginals,"ex") <- function(){
+	data(twdemcEx1)
+	sample <- stackChains(twdemcEx1)
+	sample0 <- sample[ sample[,1] >= quantile(sample[,1],0.05), ]
+	#simple plot
+	plotMarginal2D( sample0, "a", "b", minN=1 )	# actually not a marginal
+	
+	# kernel density regression estimate
+	#mtrace(plot2DKDMarginals)
+	tmp2 <- plot2DKDMarginals( rLogLik ~ a+b, sample0, dim=20  )
+	
+	#refine plot
+	xy <- apply(do.call(cbind,dimnames(tmp2)),2,as.numeric  )
+	twPlot2DFun.contour( xy[,1], xy[,2], tmp2, xdiv=nrow(tmp2)
+		, xlab="Intercept a"
+		, ylab="Slope b"
+		, key.title=title(sub="Log-Like-\nlihood\n\n")
+		, color.palette=function(n){rev(heat.colors(n))
+	})
+}
+
+
 
 
