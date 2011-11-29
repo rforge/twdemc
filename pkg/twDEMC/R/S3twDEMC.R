580baadc1b0634bf1c09559050eaa0c5401e9c9d
tomaschwutz <tomaschwutz@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-29 08:35:25 +0000

    listed S3 methods for twDEMC adapted to block sampling and parms format
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/twdemc@112 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/twDEMC/R/S3twDEMC.R
+++ pkg/twDEMC/R/S3twDEMC.R
@@ -546,7 +546,8 @@ popApplyTwDEMC <- function(
 				iChains <-  iPop0*nChainsP + (1:nChainsP)
 				FUN(x[,iChains ,drop=FALSE],...) 		
 			})
-		else if( ldim==3 ) 
+		else if( ldim==3 )
+			#iPop0=0
 			lapply( (0:(nPop-1)), function(iPop0){
 					iChains <-  iPop0*nChainsP + (1:nChainsP)
 					FUN(x[,,iChains ,drop=FALSE],...) 		
@@ -562,18 +563,24 @@ attr(popApplyTwDEMC,"ex") <- function(){
 	# mean logDen for each case, i.e. step, by population
 	nPop=getNPops(ex1c)
 	#mtrace(popApplyTwDEMC)
-	popApplyTwDEMC( ex1c$logDen, nPop=nPop, apply, 1, mean )	
-	# stack logDen for each population
-	popApplyTwDEMC( ex1c$logDen, nPop=nPop, as.vector )
+	# applied to a matrix: pops in columns
+	popApplyTwDEMC( ex1c$logDen[,1,], nPop=nPop, apply, 1, mean )
+	# applied to a 3d array: pops in 3rd dimension
+	tmp <- popApplyTwDEMC( ex1c$logDen, nPop=nPop, apply, 1:2, mean )
+	str(tmp)
+	
+	# stack logDen of block 1 for each population
+	tmp <- popApplyTwDEMC( ex1c$logDen[,1,], nPop=nPop, as.vector )
+	str(tmp)
 	#stack param columns by population
-	(tmp <- popApplyTwDEMC( ex1c$parms, nPop=nPop, function(x){ abind(twListArrDim(x),along=2) }))	
+	str(ex1c$parms)	#26 cases, 2 parameters, 2*4=8 chains
+	tmp <- popApplyTwDEMC( ex1c$parms, nPop=nPop, function(x){ abind(twListArrDim(x),along=1) })
+	all.equal( c(nrow(ex1c$parms)*4, 2,2), dim(tmp))
 }
 
 setMethodS3("stackChainsPop","twDEMC", function( 
 		### Combine MarkovChains of each population of a twDEMC. 
 		x
-		,...
-		,varInRows=FALSE	##<< set to TRUE if rows hold variables and columns steps (as in Zinit of twDEMC), defaults to variables in columns
 	){
 		#stackChainsPop.twDEMC
 		##seealso<<   
@@ -581,38 +588,23 @@ setMethodS3("stackChainsPop","twDEMC", function(
 		## \code{\link{subChains.twDEMC}}
 		# stack logDen for each population
 		nPop = getNPops(x)
-		logDen <- popApplyTwDEMC( x$logDen, nPop=nPop, as.vector )
-		#stack param columns by population
-		if( varInRows ){
-			rLogDen3 <- array( logDen, dim=c(1, nrow(logDen), ncol(logDen) ))
-			parms <- popApplyTwDEMC( x$parms, nPop=nPop, function(xp){ abind(twListArrDim(xp),along=2) })	
-			res <- structure( abind(rLogDen3,parms,along=1), dimnames=list(parms=c("logDen",rownames(x$parms)),step=NULL,pops=NULL))
-		}else{
-			rLogDen3 <- array( logDen, dim=c(nrow(logDen),1,ncol(logDen)))
-			parms <- popApplyTwDEMC( x$parms, nPop=nPop, function(xp){ t(abind(twListArrDim(xp),along=2)) })	
-			res <- structure( abind(logDen,parms,along=2), dimnames=list(step=NULL,parms=c("logDen",rownames(x$parms)),pops=NULL))
-		}
+		logDen <- popApplyTwDEMC( x$logDen, nPop=nPop, function(x){ abind(twListArrDim(x),along=1) })
+		#logDen <- popApplyTwDEMC( x$logDen, nPop=nPop, as.vector )
+		parms <- popApplyTwDEMC( x$parms, nPop=nPop, function(x){ abind(twListArrDim(x),along=1) })		
+		res <- abind(logDen,parms,along=2)
+		attr(res,"nBlock") <- ncol(logDen)
 		res
 		### Array with first column the logDensity logDen and the remaining columns the variables
 		### , rows are steps, third dimension is the population 
 		### (but see argument \code{varInRows}
 	})
 attr(stackChainsPop.twDEMC,"ex") <- function(){
-	data(ex1c)
+	data(twdemcEx1)
+	ex1c <- concatPops(twdemcEx1)
 	res <- stackChainsPop(ex1c)
 	str(res)
 	(tmp1 <- head(res[,,1]))
-	
-	res2 <- stackChainsPop(ex1c, varInRows=TRUE)
-	str(res2)
-	(tmp2 <- head(t(res2[,,1])))
-	
-	identical( tmp1, tmp2)
 }
-
-
-
-
 
 setMethodS3("thinN","mcmc.list", function( 
 	### Thin x so that each chain consists of about nThinnedRecords
@@ -627,92 +619,10 @@ setMethodS3("thinN","mcmc.list", function(
 	### Thinned x
 })
 
-getAcceptedPos.twDEMCProps <- function(
-	### Calculate the accepted state corresponding to the proposal.
-	Y	##<< matrix of proposals with row "accepted" and first step (column) initial state, and third dimension chains.
-	,acceptedRowName = "accepted"
-){
-	#j=1
-	# set the accepted state of the first entry to TRUE so that a start is given 
-	Y[acceptedRowName,1,] <- 1
-	tmp.f <- function(j){		#j is the chain, i.e. column
-		acc <- which( Y[acceptedRowName,,j,drop=TRUE] != 0)
-		accl <- diff(c(acc, ncol(Y)+1))
-		rep(acc,accl)
-	}	
-	acceptedPos <- sapply(1:dim(Y)[3], tmp.f) 
-	#acceptPos for an accepted proposal is the proposal step itselv, we need the previous one
-	if( is.matrix(acceptedPos) )
-		prevAcceptedPos <- acceptedPos[c(1,1:(nrow(acceptedPos)-1)),]
-	else
-		# sapply reduces the case for only 1 step to a vector
-		prevAcceptedPos <- matrix(acceptedPos,nrow=1)
-	prevAcceptedPos[1,] <- NA
-	prevAcceptedPos
-	### matrix of steps (steps x chains) whose state was the accepted state when evaluating proposal (row of Y)
-	### the first step is NA because there is no previous accepted state
-}
-
-getDiffLogDen.twDEMCProps <- function(
-	### Extract the Differences in LogDensity between accepted states and proposals.
-	Y					##<< matrix of proposals with row "accepted" and first step (column) initial state, rows: results components of fLogDen and third dimension chains.
-	,resCols			##<< the rows of Y with result components, either names or positions
-	,nLastSteps = 128	##<< number of last steps of Y for which to extract diffs
-	,temp=1				##<< numeric matrix (resComp x pop): the temperature applied to the difference of LogDens
-	,...				##<< further arguments passed to \code{\link{getAcceptedPos.twDEMCProps}}
-){
-	#work with positions
-	if( is.character(resCols) ) resCols <- match(resCols,rownames(Y))
-	# make sure that temp has all resCols components 
-	if( 1 < length(temp)) temp <- temp[rownames(Y)[resCols],] else temp <- matrix(temp,ncol=dim(Y)[3],nrow=1)
-	nPopsChain <- dim(Y)[3] %/% ncol(temp)
-	# constrain to the nLastSteps+1 rows
-	YL <- if( nLastSteps+1 >= ncol(Y)) Y else Y[,ncol(Y)+1-((nLastSteps+1):1),,drop=FALSE]
-	#test 1d case: YL <- Y[,min(ncol(Y),nLastSteps+1),,drop=FALSE]
-	#mtrace(getAcceptedPos.twDEMCProps)
-	acceptedPos <- getAcceptedPos.twDEMCProps(YL,...) #index of parameter vector that correponds to the currently accepted for vector at given row
-	diffLogDen <- abind(lapply( 1:dim(YL)[3], function(j){ 	adrop((YL[resCols,-1,j,drop=FALSE] - YL[resCols,acceptedPos[-1,j],j,drop=FALSE])/temp[,(j-1)%/%nPopsChain+1],3)}),rev.along=0)
-	diffLogDen
-	### numeric array ( component x nLastSteps x chain ) of Lp-La, the L
-}
-
-replaceNonFiniteDiffLogDens <- function(
-	### For each component replace NAs by sample of others and non-Finite values by minimum of others  
-	diffLogDen			##<< array( streams x steps) Lp-La see \code{\link{getDiffLogDen.twDEMCProps}}
-	,doConstrainNeg=FALSE	##<< if given, density of accepted jumps (positive) is constrained to 0
-){
-	d <- t(apply( diffLogDen,1,function(ds){
-				boNA <- is.na(ds)
-				nNA <- sum(boNA)
-				if(0<nNA ){
-					dsNonNA <- ds[!boNA]
-					ds[boNA] <- sample( dsNonNA, nNA, replace=TRUE)
-				}
-				boNonFinite <- !is.finite(ds)
-				if( 0<sum(boNonFinite)){
-					ds[boNonFinite] <- min(ds[!boNonFinite])
-				}
-				ds
-			}))
-	dimnames(d) <- dimnames(diffLogDen)
-	if( doConstrainNeg )
-		d[d>0] <- 0
-	d
-}
-
-sampleAcceptedFixedTempDiffLogDens <- function(
-	### Calculate diffLogDen for fixed temperature components and return subset of accepted ones 
-	diffLogDen			##<< array( streams x steps) Lp-La see \code{\link{getDiffLogDen.twDEMCProps}}
-	,TFix=numeric(0)
-){
-	dTFix <- colSums( diffLogDen[names(TFix),,drop=FALSE]/TFix )
-	acceptedFixed <- dTFix>log(runif(ncol(diffLogDen)))
-	#pa <- sum(acceptedFixed)/nj
-	diffLogDen[,acceptedFixed,drop=FALSE]
-}
 
 
 .tmp.f <- function(){
+	#not adapted to block sampling yet
 	setMethodS3("replacePops","twDEMC", function( 
 			### replaces several populations by other ones
 			x
---
+++ pkg/twDEMC/R/acceptedPosY.R
@@ -0,0 +1,89 @@
+# XX not adapted to block sampling yet
+
+.tmp.f <- function(){
+getAcceptedPos.twDEMCProps <- function(
+	### Calculate the accepted state corresponding to the proposal.
+	Y	##<< matrix of proposals with row "accepted" and first step (column) initial state, and third dimension chains.
+	,acceptedRowName = "accepted"
+){
+	#j=1
+	# set the accepted state of the first entry to TRUE so that a start is given 
+	Y[acceptedRowName,1,] <- 1
+	tmp.f <- function(j){		#j is the chain, i.e. column
+		acc <- which( Y[acceptedRowName,,j,drop=TRUE] != 0)
+		accl <- diff(c(acc, ncol(Y)+1))
+		rep(acc,accl)
+	}	
+	acceptedPos <- sapply(1:dim(Y)[3], tmp.f) 
+	#acceptPos for an accepted proposal is the proposal step itselv, we need the previous one
+	if( is.matrix(acceptedPos) )
+		prevAcceptedPos <- acceptedPos[c(1,1:(nrow(acceptedPos)-1)),]
+	else
+		# sapply reduces the case for only 1 step to a vector
+		prevAcceptedPos <- matrix(acceptedPos,nrow=1)
+	prevAcceptedPos[1,] <- NA
+	prevAcceptedPos
+	### matrix of steps (steps x chains) whose state was the accepted state when evaluating proposal (row of Y)
+	### the first step is NA because there is no previous accepted state
+}
+
+getDiffLogDen.twDEMCProps <- function(
+	### Extract the Differences in LogDensity between accepted states and proposals.
+	Y					##<< matrix of proposals with row "accepted" and first step (column) initial state, rows: results components of fLogDen and third dimension chains.
+	,resCols			##<< the rows of Y with result components, either names or positions
+	,nLastSteps = 128	##<< number of last steps of Y for which to extract diffs
+	,temp=1				##<< numeric matrix (resComp x pop): the temperature applied to the difference of LogDens
+	,...				##<< further arguments passed to \code{\link{getAcceptedPos.twDEMCProps}}
+){
+	#work with positions
+	if( is.character(resCols) ) resCols <- match(resCols,rownames(Y))
+	# make sure that temp has all resCols components 
+	if( 1 < length(temp)) temp <- temp[rownames(Y)[resCols],] else temp <- matrix(temp,ncol=dim(Y)[3],nrow=1)
+	nPopsChain <- dim(Y)[3] %/% ncol(temp)
+	# constrain to the nLastSteps+1 rows
+	YL <- if( nLastSteps+1 >= ncol(Y)) Y else Y[,ncol(Y)+1-((nLastSteps+1):1),,drop=FALSE]
+	#test 1d case: YL <- Y[,min(ncol(Y),nLastSteps+1),,drop=FALSE]
+	#mtrace(getAcceptedPos.twDEMCProps)
+	acceptedPos <- getAcceptedPos.twDEMCProps(YL,...) #index of parameter vector that correponds to the currently accepted for vector at given row
+	diffLogDen <- abind(lapply( 1:dim(YL)[3], function(j){ 	adrop((YL[resCols,-1,j,drop=FALSE] - YL[resCols,acceptedPos[-1,j],j,drop=FALSE])/temp[,(j-1)%/%nPopsChain+1],3)}),rev.along=0)
+	diffLogDen
+	### numeric array ( component x nLastSteps x chain ) of Lp-La, the L
+}
+
+
+replaceNonFiniteDiffLogDens <- function(
+	### For each component replace NAs by sample of others and non-Finite values by minimum of others  
+	diffLogDen			##<< array( streams x steps) Lp-La see \code{\link{getDiffLogDen.twDEMCProps}}
+	,doConstrainNeg=FALSE	##<< if given, density of accepted jumps (positive) is constrained to 0
+){
+	d <- t(apply( diffLogDen,1,function(ds){
+				boNA <- is.na(ds)
+				nNA <- sum(boNA)
+				if(0<nNA ){
+					dsNonNA <- ds[!boNA]
+					ds[boNA] <- sample( dsNonNA, nNA, replace=TRUE)
+				}
+				boNonFinite <- !is.finite(ds)
+				if( 0<sum(boNonFinite)){
+					ds[boNonFinite] <- min(ds[!boNonFinite])
+				}
+				ds
+			}))
+	dimnames(d) <- dimnames(diffLogDen)
+	if( doConstrainNeg )
+		d[d>0] <- 0
+	d
+}
+
+sampleAcceptedFixedTempDiffLogDens <- function(
+	### Calculate diffLogDen for fixed temperature components and return subset of accepted ones 
+	diffLogDen			##<< array( streams x steps) Lp-La see \code{\link{getDiffLogDen.twDEMCProps}}
+	,TFix=numeric(0)
+){
+	dTFix <- colSums( diffLogDen[names(TFix),,drop=FALSE]/TFix )
+	acceptedFixed <- dTFix>log(runif(ncol(diffLogDen)))
+	#pa <- sum(acceptedFixed)/nj
+	diffLogDen[,acceptedFixed,drop=FALSE]
+}
+
+} #.tmp.f
